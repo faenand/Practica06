825c7668aad708ffa73478a552aebd59bb31e631
Fernando Mendoza Eslava <88646364+faenand@users.noreply.github.com>
2021-11-18 23:32:52 -0500

    Update Referencia.java

--- Referencia.java
+++ Referencia.java
@@ -9,15 +9,16 @@ public class Referencia {
     public static void main(String args[]) {
         String[] array = new String[1];
         change(array);
-        System.out.println("El valor del array es: " + array[0]);
+        System.out.println("El estado del array es: " + array[0]);
     }
     //Metodo que cambia el valor al array
-    //Al pasar argumentos por referencia, los elementos del arreglo pueden cambiarse y en este caso se imprime el mensaje
+    //Al pasar argumentos por referencia, el objeto en si no puede cambiarse e imprime null
     public static void change(String[] array) {
        array = new String[1];
        array[0] = "Se realizo el cambio de valor";
     }
 }
+
 
 
 
